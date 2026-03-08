1b0978b065376497d78e86ab4bb8f41ffec5b341
tedraber <tedraber09@gmail.com>
2026-03-07 20:55:07 -0500

    my initial nixos config

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "NixOS system config";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+  let
+    system = "x86_64-linux";
+  in {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      inherit system;
+
+      modules = [
+        ./configuration.nix
+
+        home-manager.nixosModules.home-manager
+
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+
+          home-manager.users.ted = ./home.nix;
+
+          home-manager.extraSpecialArgs = { inherit (self) inputs; };
+        }
+      ];
+    };
+  };
+}
