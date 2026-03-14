2d9bcac60fc118c18314945c1e8eeb64dcc982e3
tedraber <tedraber09@gmail.com>
2026-03-14 14:48:42 -0400

    fixed ambxst shell stuff

--- hyprland.nix
+++ hyprland.nix
@@ -1,4 +1,4 @@
-{config, pkgs, ...}:
+{config, pkgs, inputs, ...}:
 
 {
   wayland.windowManager.hyprland = {
@@ -23,7 +23,7 @@
       };
 
       exec-once = [
-        "ambxst"
+        "bash -c ambxst"
       ];
       
       env = [
@@ -42,34 +42,9 @@
       "$mod" = "SUPER";
 
       bind = [
-        "$mod, T, exec, ghostty"
         "$mod, Q, killactive"
         "$mod, SPACE, exec, rofi -show drun"
         "$mod, B, exec, helium"
-        "$mod, E, exec, nautilus --new-window"
-
-        # Move focus/windows with keys
-        "$mod, H, movefocus, l"
-        "$mod, L, movefocus, r"
-        "$mod, K, movefocus, u"
-        "$mod, J, movefocus, d"
-
-        # Resize windows with keys
-        "$mod SHIFT, left, resizeactive, -20 0"
-        "$mod SHIFT, right, resizeactive, 20 0"
-        "$mod SHIFT, up, resizeactive, 0 -20"
-        "$mod SHIFT, down, resizeactive, 0 20"
-
-        # Move active window with mod + SHIFT + hjkl
-        "$mod SHIFT, H, movewindow, l"
-        "$mod SHIFT, L, movewindow, r"
-        "$mod SHIFT, K, movewindow, u"
-        "$mod SHIFT, J, movewindow, d"
-
-        "$mod, P, pseudo," # toggle pseudo-tiling
-        "$mod, G, togglesplit," # change split direction (horizontal/vertical)
-
-        "$mod SHIFT, F, fullscreen"
       ];
 
       bindm = [
